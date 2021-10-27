7333cfb45a99ac5cb2f2f344338b3c43e63cdfec
chrismerkle19 <82123634+chrismerkle19@users.noreply.github.com>
2021-10-26 23:39:32 -0500

    Update mainapp.py
    fixed slack-alert

--- mainapp.py
+++ mainapp.py
@@ -115,8 +115,8 @@ def md5(string):
 @app.route('/slack-alert/<string:message>')
 def slackalert(message):
     payload = '{"text":"%s"}' % message
-    response = requests.post('https://hooks.slack.com/services/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',data=payload)
-    print(response.txt)
+    requests.post('https://hooks.slack.com/services/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',data=payload)
+    return('ok')
     
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
