e9481d2c465c99ce2ece0bad30e60c9265864d0d
chrismerkle19 <82123634+chrismerkle19@users.noreply.github.com>
2021-11-23 16:17:26 -0600

    Removed useless for loop

--- CLI.py
+++ CLI.py
@@ -1,14 +1,13 @@
 import sys
 import requests
 
-for arg in sys.argv:
-    URL = ('http://localhost:5000/' + sys.argv[1] + '/' + sys.argv[2])
+URL = ('http://localhost:5000/' + sys.argv[1] + '/' + sys.argv[2])
 
-    if sys.argv[1] == 'man':
-        print ("Enter the url of your request (app *** ***)")
-    else:
-        result = requests.get(URL)
+if sys.argv[1] == 'man':
+    print ("Enter the url of your request (app *** ***)")
+else:
+    result = requests.get(URL)
 
-        results_text = result.json()
+    results_text = result.json()
 
-        print (results_text)
+    print (results_text)
